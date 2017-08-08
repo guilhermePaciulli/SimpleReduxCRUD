c90996c47a4027ea95bd6796b0565b0461c92789
guilhermePaciulli <gpaciulli100@gmail.com>
2017-08-08 09:45:35 -0300

    Inspect action created

---
+++ ReduxTest/InspectAction.swift
@@ -0,0 +1,19 @@
+//
+//  InspectAction.swift
+//  ReduxTest
+//
+//  Created by Guilherme Paciulli on 08/08/17.
+//  Copyright © 2017 Guilherme Paciulli. All rights reserved.
+//
+
+import Foundation
+import ReSwift
+import UIKit
+
+struct InspectAction: Action {
+    
+    var name: String
+    var price: Float
+    var picture: UIImage
+    
+}
