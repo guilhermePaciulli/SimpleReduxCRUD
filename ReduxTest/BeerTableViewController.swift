801b4b9938f0f5667cd5abbf52b2f35ee06cfa04
guilhermePaciulli <gpaciulli100@gmail.com>
2017-08-11 14:07:26 -0300

    Edit view controller implemented

--- ReduxTest/BeerTableViewController.swift
+++ ReduxTest/BeerTableViewController.swift
@@ -23,7 +23,7 @@ class BeerTableViewController: UITableViewController {
         beers = store.state.readingState.beers
         self.tableView.reloadData()
     }
-
+    
     @IBAction func addBeer(_ sender: UIBarButtonItem) {
         if let createViewController = self.storyboard?.instantiateViewController(withIdentifier: "create") as? CreateViewController {
             createViewController.modalTransitionStyle = .crossDissolve
@@ -32,15 +32,15 @@ class BeerTableViewController: UITableViewController {
         
     }
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return beers.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "beerCell", for: indexPath)
         
@@ -48,20 +48,29 @@ class BeerTableViewController: UITableViewController {
         cell.textLabel?.text = beer.name
         cell.imageView?.image = beer.getPicture()
         cell.detailTextLabel?.text = "$" + String(describing: beer.price)
-
+        
         return cell
     }
-
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
-
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let inspectViewController = self.storyboard?.instantiateViewController(withIdentifier: "inspect") as? InspectViewController {
+            let selectedBeer = self.beers[indexPath.row]
+            inspectViewController.beer = selectedBeer
+            inspectViewController.modalTransitionStyle = .crossDissolve
+            self.present(inspectViewController, animated: true, completion: nil)
+        }
+    }
+    
 }
--- ReduxTest/EditViewController.swift
+++ ReduxTest/EditViewController.swift
@@ -9,27 +9,133 @@
 import UIKit
 
 class EditViewController: UIViewController {
+    
+    var beer: Beer?
+    let picker = UIImagePickerController()
+    
+    @IBOutlet weak var popUpView: UIView!
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var price: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        picker.delegate = self
+        picture.isUserInteractionEnabled = true
+        self.popUpView.layer.cornerRadius = 10
+        self.popUpView.clipsToBounds = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        if let beerName = self.beer?.name {
+            if let beerPrice = self.beer?.price {
+                if let picture = self.beer?.getPicture() {
+                    self.name.text = beerName
+                    self.price.text = "$\(beerPrice)"
+                    self.picture.image = picture
+                }
+            }
+        }
     }
-    */
+    
+    @IBAction func changePic(_ sender: Any) {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let shoot = UIAlertAction(title: "Shoot photo", style: .default, handler: { [unowned self] action in
+            self.shootPhoto() })
+        let select = UIAlertAction(title: "Pick photo", style: .default, handler: { [unowned self] action in
+            self.showPickerPhoto() })
+        alert.addAction(cancel)
+        alert.addAction(select)
+        alert.addAction(shoot)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
 
+    @IBAction func ok(_ sender: Any) {
+        if allFieldsAreValid() {
+            if let name = self.name.text {
+                if let price = (self.price.text as NSString?)?.floatValue {
+                    if let picture = self.picture.image {
+                        store.dispatch(UpdateAction(beer: self.beer!,
+                                                    name: name,
+                                                    price: price,
+                                                    picture: picture))
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: {})
+    }
+    
+    func allFieldsAreValid() -> Bool {
+        if self.name.text == nil ||
+            self.picture.image == nil ||
+            self.price.text == nil ||
+            (self.price.text! as NSString).floatValue.isZero {
+            
+            let alert = UIAlertController(title: "All fields are needed", message: nil, preferredStyle: .alert)
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alert.addAction(cancel)
+            alert.addAction(ok)
+            
+            self.present(alert, animated: true, completion: nil)
+            
+            return false
+        }
+        return true
+    }
+}
+
+extension EditViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func showPickerPhoto() {
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        picker.modalPresentationStyle = .popover
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func shootPhoto() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.allowsEditing = false
+            picker.sourceType = UIImagePickerControllerSourceType.camera
+            picker.cameraCaptureMode = .photo
+            picker.modalPresentationStyle = .fullScreen
+            present(picker, animated: true, completion: nil)
+        } else {
+            noCamera()
+        }
+    }
+    
+    func noCamera(){
+        let alertVC = UIAlertController(
+            title: "No Camera",
+            message: "Sorry, this device has no camera",
+            preferredStyle: .alert)
+        let okAction = UIAlertAction(
+            title: "Ok",
+            style:.default,
+            handler: nil)
+        alertVC.addAction(okAction)
+        present(alertVC, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.picture.image = image
+        }
+        dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
+    }
+    
 }
