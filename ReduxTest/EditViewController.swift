06bd613c89f7e71b5342206eb953a373aa0ba3a7
guilhermePaciulli <gpaciulli100@gmail.com>
2017-08-11 14:01:27 -0300

    Inspect view implemented

---
+++ ReduxTest/EditViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EditViewController.swift
+//  ReduxTest
+//
+//  Created by Guilherme Paciulli on 11/08/17.
+//  Copyright © 2017 Guilherme Paciulli. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ReduxTest/InspectViewController.swift
@@ -0,0 +1,51 @@
+//
+//  InspectViewController.swift
+//  ReduxTest
+//
+//  Created by Guilherme Paciulli on 11/08/17.
+//  Copyright © 2017 Guilherme Paciulli. All rights reserved.
+//
+
+import UIKit
+
+class InspectViewController: UIViewController {
+    
+    var beer: Beer?
+
+    @IBOutlet weak var popUpView: UIView!
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var price: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.popUpView.layer.cornerRadius = 10
+        self.popUpView.clipsToBounds = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let beerName = self.beer?.name {
+            if let beerPrice = self.beer?.price {
+                if let picture = self.beer?.getPicture() {
+                    self.name.text = beerName
+                    self.price.text = "$\(beerPrice)"
+                    self.picture.image = picture
+                }
+            }
+        }
+    }
+
+    @IBAction func ok(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func edit(_ sender: Any) {
+        self.dismiss(animated: true, completion: {
+            if let editViewController = self.storyboard?.instantiateViewController(withIdentifier: "edit") as? EditViewController {
+                editViewController.beer = self.beer
+                editViewController.modalTransitionStyle = .crossDissolve
+                self.present(editViewController, animated: true, completion: nil)
+            }
+        })
+    }
+}
